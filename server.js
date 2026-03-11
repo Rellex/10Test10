fa920e6cdb0c63ca8ad06dbe450179dd12048d8c
Rellex <robeltshilmeyster@gmail.com>
2026-03-11 17:55:38 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ function broadcast(event, data) {
 wss.on('connection', ws => {
   ws.on('error', () => {});
 });
-const YOOKASSA_SHOP_ID = process.env.YOOKASSA_SHOP_ID || '1220854';
-const YOOKASSA_SECRET  = process.env.YOOKASSA_SECRET  || 'live_5gzVyNewIBgNBZ4X43gkUkwMlGtNK8gKecdynUVWll8';
+const YOOKASSA_SHOP_ID = process.env.YOOKASSA_SHOP_ID || '1297471';
+const YOOKASSA_SECRET  = process.env.YOOKASSA_SECRET  || 'live_HruyIJAB-UGfH7BrsNNu7fI2iyX_7cclcgOIk__2JGs';
 const YOOKASSA_API     = 'https://api.yookassa.ru/v3';
 
 app.use(cors());
