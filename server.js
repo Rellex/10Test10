acebc07a288d943ac8a3f02f2f862ef458d3834b
Rellex <robeltshilmeyster@gmail.com>
2026-03-12 20:25:27 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const YOOKASSA_SECRET  = process.env.YOOKASSA_SECRET  || 'live_HruyIJAB-UGfH7Brs
 const YOOKASSA_API     = 'https://api.yookassa.ru/v3';
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(__dirname));
 
 /* ── paths ─────────────────────────────────── */
